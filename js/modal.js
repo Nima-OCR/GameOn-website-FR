50fcc1f8f330ab9f2a239798d70bf2cd672c56c1
CyrusLeGrand <cyrus.achemenide@gmail.com>
2023-03-24 18:50:18 +0100

    fonction validateInput created & updated

--- js/modal.js
+++ js/modal.js
@@ -40,48 +40,41 @@ closeModal.addEventListener("click", closeFunction);
 
 
 /**
- *   ########################################################################################
- *       Le champ Prénom & Nom a un minimum de 2 caractères / n'est pas vide
- *   ########################################################################################
+ *   #############################################################################################
+ *       Le champ Prénom & Nom a un minimum de 2 caractères / n'est pas vide / Validation Email
+ *   #############################################################################################
  */
 
+/**
+ * Récupère les éléments HTML pour le prénom, le nom et l'email
+ * @type {HTMLElement}
+ */
 const firstNameInput = document.getElementById("first");
 const lastNameInput = document.getElementById("last");
 const emailInput = document.getElementById("email");
 
-const regexName = /^\s*(?=.*[a-zA-Zéèàùç])[a-zA-Zéèàùç]{2,}\s*$/;
-const regexEmail = /^([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
 
-firstNameInput.addEventListener("input", function () {
-  if (!regexName.test(firstNameInput.value)) {
-    firstNameInput.parentElement.setAttribute("data-error", "Le prénom doit comporter au moins 2 lettres");
-    firstNameInput.parentElement.setAttribute("data-error-visible", "true");
-  } else {
-    firstNameInput.parentElement.removeAttribute("data-error");
-    firstNameInput.parentElement.removeAttribute("data-error-visible");
-  }
-});
+/**
+ * Valide chaque champ avec une expression régulière et un message d'erreur
+ * @param {HTMLElement} input - L'élément de champ de saisie HTML à valider.
+ * @param {RegExp} regex - L'expression régulière à utiliser pour valider le champ de saisie.
+ * @param {string} errorMessage - Le message d'erreur à afficher si la saisie ne correspond pas à l'expression régulière.
+ */
 
-lastNameInput.addEventListener("input", function () {
-  if (!regexName.test(lastNameInput.value)) {
-    lastNameInput.parentElement.setAttribute("data-error", "Le nom doit comporter au moins 2 lettres");
-    lastNameInput.parentElement.setAttribute("data-error-visible", "true");
-  } else {
-    lastNameInput.parentElement.removeAttribute("data-error");
-    lastNameInput.parentElement.removeAttribute("data-error-visible");
-  }
-});
-
-emailInput.addEventListener("input", function () {
-  if (!regexEmail.test(emailInput.value)) {
-    emailInput.parentElement.setAttribute("data-error", "L'email n'est pas valide");
-    emailInput.parentElement.setAttribute("data-error-visible", "true");
-  } else {
-    emailInput.parentElement.removeAttribute("data-error");
-    emailInput.parentElement.removeAttribute("data-error-visible");
-  }
-});
+validateInput(firstNameInput, /^\s*(?=.*[a-zA-Zéèàùç])[a-zA-Zéèàùç]{2,}\s*$/, "Le prénom doit comporter au moins 2 lettres");
+validateInput(lastNameInput, /^\s*(?=.*[a-zA-Zéèàùç])[a-zA-Zéèàùç]{2,}\s*$/, "Le nom doit comporter au moins 2 lettres");
+validateInput(emailInput, /^([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/, "L'email n'est pas valide");
 
 
-
-
+/**
+ * @param {HTMLElement} input - L'élément de champ de saisie HTML à valider.
+ * @param {RegExp} regex - L'expression régulière à utiliser pour valider le champ de saisie.
+ * @param {string} errorMessage - Le message d'erreur à afficher si la saisie ne correspond pas à l'expression régulière.
+ */
+function validateInput (input, regex, errorMessage) {
+  input.addEventListener("input", function () {
+    const isValid = regex.test(input.value);
+    input.parentElement.setAttribute("data-error", isValid ? "" : errorMessage);
+    input.parentElement.setAttribute("data-error-visible", !isValid);
+  })
+}
